89ec2aadc5cedcb1367f040ab4a0eb4d6ef4b5d6
Fuatkoseoglu <fuatcankoseoglu@gmail.com>
2020-12-06 17:00:53 +0300

    functions are objects too

--- objectsCreateMethods.js
+++ objectsCreateMethods.js
@@ -2,6 +2,7 @@
 //this is a factory function it returns a new object
 //Instead of this.radius = radius
 //Because names are the same we can use radius only
+
 function createCircle(radius) {
     return {
         radius,
@@ -17,6 +18,7 @@ circle.draw();
 // Constructor Function
 // First letter should be uppercase
 // Similar to c# class
+// Uses javascripts native constructor
 function Circle(radius) {
     this.radius = radius;
     this.draw = function () {
@@ -26,3 +28,21 @@ function Circle(radius) {
 //Dont forget write new
 const anotherCircle = new Circle(1);
 anotherCircle.draw();
+
+//Functions are objects too.
+const Circle1 = new Function('radius', `
+this.radius = radius;
+    this.draw = function () {
+        console.log('functions are object too');
+    }`);
+
+const circle2 = new Circle1(1);
+circle2.draw();
+
+//we can call function with this
+// {} is empty object
+//this is same as const circle = new Circle(1);
+Circle.call({},1);
+
+//this passes array as arguments good if you have arrays.
+Circle.apply({},[1,2,3]);
