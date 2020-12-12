e26c6d09e575a5adeba7446925c1df984d310e64
Fuat-Mac <32915609+Sebiascor@users.noreply.github.com>
2020-12-12 18:07:51 +0300

    Update conditionalStatements.js
    added switch case

--- conditionalStatements.js
+++ conditionalStatements.js
@@ -4,11 +4,25 @@ let today = new Date();
 let hour = today.getHours();
 console.log(hour);
 
-if(hour >= 6 && hour < 12)
+if (hour >= 6 && hour < 12)
     console.log('Good Morning!');
-else if(hour >= 12 && hour < 18)
+else if (hour >= 12 && hour < 18)
     console.log('Good afternoon!');
 else
     console.log('Good Evening!');
+
+
+////SWITCH
+switch (hour) {
+    case (hour >= 6 && hour < 12):
+        console.log('Good Morning!');
+        break;
+    case (hour >= 12 && hour < 12):
+        console.log('Good Afternoon');
+        break;
+    default:
+        console.log('Good Evening!');
+        break;
+}
 
 
